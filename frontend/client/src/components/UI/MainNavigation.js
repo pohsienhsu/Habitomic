32f660513af430fbc4c070597b4b5633b7ed3dc8
phsu40 <phsu40@gatech.edu>
2023-05-31 21:54:07 -0700

    Create MainNavigation component for home page Navbar

---
+++ frontend/client/src/components/UI/MainNavigation.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+const MainNavigation = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Habitomic</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#habits">My Habits</Nav.Link>
+            <Nav.Link href="#profile">Profile</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default MainNavigation;
