7ec8eb15751fd6aa3f665cfac272b6324b38762d
phsu40 <phsu40@gatech.edu>
2023-06-01 21:29:42 -0700

    Create HabitsList component to display a list of habits

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,9 +1,39 @@
 import MainNavigation from "./components/UI/MainNavigation";
+import HabitsList from "./components/Habits/HabitsList";
 
 import "./App.css";
 
+const DUMMY_HABITS = [
+  {
+    title: "",
+    cue: "",
+    craving: "",
+    action: "",
+    reward: "",
+  },
+  {
+    title: "",
+    cue: "",
+    craving: "",
+    action: "",
+    reward: "",
+  },
+  {
+    title: "",
+    cue: "",
+    craving: "",
+    action: "",
+    reward: "",
+  },
+];
+
 function App() {
-  return <MainNavigation />;
+  return (
+    <>
+      <MainNavigation />
+      <HabitsList />
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/client/src/components/Habits/HabitsList.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import Badge from "react-bootstrap/Badge";
+import ListGroup from "react-bootstrap/ListGroup";
+
+import CardWrap from "../UI/CardWrap";
+
+const HabitsList = (props) => {
+  return (
+    <CardWrap>
+      <ListGroup as="ol" numbered>
+        <ListGroup.Item
+          as="li"
+          className="d-flex justify-content-between align-items-start"
+        >
+          <div className="ms-2 me-auto">
+            <div className="fw-bold">Subheading</div>
+            Cras justo odio
+          </div>
+          <Badge bg="primary" pill>
+            14
+          </Badge>
+        </ListGroup.Item>
+      </ListGroup>
+    </CardWrap>
+  );
+};
+
+export default HabitsList;
